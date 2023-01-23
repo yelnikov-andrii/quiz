637bcf43ed24a979724cf7860f4a786f5adeb143
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-01-23 15:48:14 +0200

    height 100 vh and centering quiz question

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -6,12 +6,16 @@ interface Props {
 
 export const Logo: React.FC <Props> = ({className}) => {
   return (
-    <>
-      <img 
-        src="https://involve-me.imgix.net/uploads/assets/93744c2c-4e46-439c-bf51-17ac52607418.png?q=55&dpr=1.25&w=600&fit=max" alt=''
-        className={className}
-      />
-    </>
-  );
-};
-
+      <picture className={className}>
+         <source 
+           media="(max-width:375px)" 
+           srcSet="https://involve-me.imgix.net/uploads/assets/8aee16fe-53ce-4f2d-9e6f-5fce12f00294.png?q=55&dpr=2&w=472&fit=max" 
+        />
+         <img 
+           src="https://involve-me.imgix.net/uploads/assets/93744c2c-4e46-439c-bf51-17ac52607418.png?q=55&dpr=1.25&w=600&fit=max" 
+           alt="Flowers" 
+           style={{width: '100%'}}
+          />
+      </picture>
+  )
+}

5fec14bbcbcb965b2ad12cd00dab1a8c6544a898
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-01-23 17:53:53 +0200

    height 100 vh and centering quiz question

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../images/logo.png';
 
 interface Props {
   className: string;
@@ -7,18 +6,10 @@ interface Props {
 
 export const Logo: React.FC <Props> = ({className}) => {
   return (
-      <picture className={className}>
-         <source 
-           media="(max-width:375px)" 
-          //  srcSet="https://involve-me.imgix.net/uploads/assets/8aee16fe-53ce-4f2d-9e6f-5fce12f00294.png?q=55&dpr=2&w=472&fit=max"
-          srcSet={logo}
-           style={{width: '70px', height: '17px', margin: '0 0 10px 0'}} 
-        />
          <img 
            src="https://involve-me.imgix.net/uploads/assets/93744c2c-4e46-439c-bf51-17ac52607418.png?q=55&dpr=1.25&w=600&fit=max" 
            alt="Flowers" 
            className={className}
           />
-      </picture>
   )
 }

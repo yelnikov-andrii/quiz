abc35028becc481a003cab6c1b65dc7781b1bc51
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-01-23 21:19:33 +0200

    added gtm

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-5CT8BX4'
+}
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
